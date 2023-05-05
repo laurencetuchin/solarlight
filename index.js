b11adb664877cf7e33e5801e05b1efc95da24b75
laurencetuchin <laurencetuchin@gmail.com>
2023-05-05 16:28:35 +1000

    Update images with responsive design

--- index.js
+++ index.js
@@ -23,3 +23,32 @@ var x = setInterval(function () {
 		document.querySelector(".countdown-timer").innerHTML = "EXPIRED";
 	}
 }, 1000);
+
+function getDeliveryDate(deliveryDays) {
+	// Get the current date
+	const today = new Date();
+	// Calculate the delivery date by adding the delivery days to the current date
+	let deliveryDate = new Date(
+		today.getTime() + deliveryDays * 24 * 60 * 60 * 1000
+	);
+	// If the delivery date is on a weekend, adjust it to the nearest weekday
+	if (deliveryDate.getDay() == 6) {
+		deliveryDate.setDate(deliveryDate.getDate() + 2);
+	} else if (deliveryDate.getDay() == 0) {
+		deliveryDate.setDate(deliveryDate.getDate() + 1);
+	}
+	// Format the delivery date as "Day, Month Date"
+	const options = { weekday: "long", month: "long", day: "numeric" };
+	const formattedDate = deliveryDate.toLocaleDateString("en-US", options);
+	// Return the formatted delivery date
+	return formattedDate;
+}
+
+function setDeliveryDate() {
+	const deliveryDate = getDeliveryDate(4); // assuming 4 days of delivery time
+	const deliveryDateElement = document.querySelector("#delivery-date");
+	deliveryDateElement.textContent = deliveryDate;
+}
+
+// Call the setDeliveryDate function as soon as the page is loaded
+window.addEventListener("load", setDeliveryDate);
